72e6bf02bb33ec722893089f9eea3adfa49b0139
wirawan-mahardika <wirawanmahardika10@gmail.com>
2023-01-17 01:42:24 +0700

    portfolio mobile version part

---
+++ svg/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'quicksand': ["'Quicksand', sans-serif"]
+      },
+      colors: {
+        'fogra': '#0B0A0A'
+      }
+    },
+  },
+  plugins: [],
+}
